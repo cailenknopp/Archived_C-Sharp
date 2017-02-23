344bf29b303407523c273c71f7d3e9e218aeb4ef
Cailen Knopp <code@cailen.net>
2017-02-23 14:51:46 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//This is it. Let the c# journey begin - hopefully it will bring me to Unity game development, and the creation of some
+//awesome video games.
+//February 20th, 2017.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.Read();
+        }
+    }
+}
