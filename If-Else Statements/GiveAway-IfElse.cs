df0312c7da2a6bc89aaa1bd9139d0b149e4a19a0
Cailen Knopp <code@cailen.net>
2017-02-23 14:48:02 -0700

    Add files via upload

---
+++ If-Else Statements/GiveAway-IfElse.cs
@@ -0,0 +1,44 @@
+﻿//Learning how to use reuse strings, and if statements. February 21st, 2017.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Cailen's Curtain Game");
+            Console.Write("Curtain 1, 2, or 3?: ");
+            string usersChoice = Console.ReadLine();
+
+            string message = "";
+
+            if (usersChoice == "1")
+                message = "You win a new car!";
+            else if (usersChoice == "2")
+                message = "You don't win anything!";
+            else if (usersChoice == "3")
+                message = "You win... nothing, loser!";
+            else
+                message = "I don't understand.";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Cailen's Big Giveaway");
+            Console.WriteLine("Please select curtain 1, 2, or 3.");
+            Console.Write("Your selection is?: ");
+            string userChoice = Console.ReadLine();
+            string message = (userChoice == "1") ? "a brand new car!" : "nothing because you are a loser.";
+
+            Console.WriteLine("Your choice was {0}, therefore you win {1}", userChoice, message);
+            Console.ReadLine();
+        }
+    }
+}
