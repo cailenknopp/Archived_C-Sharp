34687a7e889d9ca7045ff7950916808347803c8d
Cailen Knopp <code@cailen.net>
2017-02-23 14:49:57 -0700

    Add files via upload

---
+++ If-Else Statements/Program.cs
@@ -0,0 +1,22 @@
+﻿//This will play one thousand beeps. February 21st, 2017.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Beep(600, 200);
+                Console.WriteLine("Beep #" + i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
