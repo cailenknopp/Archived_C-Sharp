53617beab9db9400881e36399c1dfff1f681756b
Cailen Knopp <code@cailen.net>
2017-02-23 14:56:10 -0700

    Add files via upload

---
+++ Strings/TestingStrings.cs
@@ -0,0 +1,28 @@
+﻿//Working with strings, February 20th, 2017.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            Console.Write("Your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            Console.Write("Your last name: ");
+            string myLastName;
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName +".");
+            Console.ReadLine();
+        }
+    }
+}
