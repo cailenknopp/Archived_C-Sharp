42ed27ba883e88493a7c8a44405e48141676ffe4
Cailen Knopp <code@cailen.net>
2017-02-23 15:16:24 -0700

    Add files via upload

---
+++ Variables/Varaibles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Variables, February 20th, 2017
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            x = 5;
+            y = x + 5;
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+        }
+    }
+}
